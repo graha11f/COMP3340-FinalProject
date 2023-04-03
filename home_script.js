7f52130d6326f97bd2ec0204878bebe0682f7179
maren11n <82608019+maren11n@users.noreply.github.com>
2023-04-03 15:15:04 -0400

    Add Contactus button script
    - Added script that scrolls user to end of page

--- home_script.js
+++ home_script.js
@@ -1,1 +1,6 @@
 console.log("Script Runnable by Group 17")
+
+//Code for contact us button, moves user to bottom of page where contact info is found
+function scrollToBottom() {
+  window.scrollTo(0,document.body.scrollHeight);
+}
